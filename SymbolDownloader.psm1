27c1b05e9b504cd84ec6a281777fdee2bafad0a7
michael dudley <michael.dudley@improvingenterprises.com>
2017-07-12 14:59:39 -0500

    creating the module

--- SymbolDownloader.psm1
+++ SymbolDownloader.psm1
@@ -1,1 +1,8 @@
-﻿
+﻿$ModuleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+"$ModuleRoot\Source\*.ps1" |
+    Resolve-Path |
+    Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
+    ForEach-Object { . $_.ProviderPath }
+
+Export-ModuleMember -function Get-Symbols
